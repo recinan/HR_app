bf12ae6b0ae244345fc149a4735993c94e3cd7eb
Recep Inan <recinan.ceng@gmail.com>
2025-04-29 18:22:28 +0300

    Application Serializer Update

--- hr_app_api/application/serializers.py
+++ hr_app_api/application/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import Application
 
 class ApplicationSerializer(serializers.ModelSerializer):
+    jobTitle = serializers.CharField(max_length=50)
     cvFilePath = serializers.FileField()
+    description = serializers.CharField(max_length=256, allow_blank=True)
     class Meta:
         model = Application
         fields = ['jobTitle','cvFilePath','description']
