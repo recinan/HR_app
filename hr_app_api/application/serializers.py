218699b4b4e5f225aea31b95d507bb6fc6e9b1e1
Recep Inan <recinan.ceng@gmail.com>
2025-05-07 22:13:01 +0300

    download cv file

--- hr_app_api/application/serializers.py
+++ hr_app_api/application/serializers.py
@@ -5,12 +5,6 @@ class ApplicationSerializer(serializers.ModelSerializer):
     jobTitle = serializers.CharField(max_length=50)
     cvFilePath = serializers.FileField(required=True)
     description = serializers.CharField(max_length=256, allow_blank=True)
-    user_email = serializers.SerializerMethodField()
     class Meta:
         model = Application
-        fields = ['id','user_email','jobTitle','cvFilePath','description','applied_at']
-
-    def get_user_email(self, obj):
-        return f"{obj.user.email}"
-
-
+        fields = ['id','jobTitle','cvFilePath','description','applied_at']
